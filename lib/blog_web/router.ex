e97578b2e30110ded45df52f143447bd01c0ccc5
pavel <pavel.sergutin@cronix.ms>
2018-04-24 15:32:00 +0300

    /my_user

--- lib/blog_web/router.ex
+++ lib/blog_web/router.ex
@@ -1,8 +1,14 @@
 defmodule BlogWeb.Router do
   use BlogWeb, :router
 
+  alias Blog.Guardian
+
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  pipeline :jwt_authenticated do
+    plug Guardian.AuthPipeline
   end
 
   scope "/api/v1", BlogWeb do
@@ -12,4 +18,11 @@ defmodule BlogWeb.Router do
     post "/register", UserController, :create
     post "/login", UserController, :login
   end
+
+  scope "/api/v1", BlogWeb do
+    pipe_through [:api, :jwt_authenticated]
+
+    get "/my_user", UserController, :show
+  end
+
 end
